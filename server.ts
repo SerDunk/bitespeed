45717863686020eb229ccec240e493795f49d70a
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-05-31 02:55:55 +0530

    updated PORT

--- server.ts
+++ server.ts
@@ -198,7 +198,7 @@ app.post("/identify", async (req: Request, res: Response): Promise<any> => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running");
 });
