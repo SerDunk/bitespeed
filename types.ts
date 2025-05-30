9202141f6e7b1e2febc6fb5fd99d39e8aab21675
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-05-31 01:09:09 +0530

    Typescript setup and intial server run

---
+++ types.ts
@@ -0,0 +1,13 @@
+export interface IdentifyRequest {
+  email?: string;
+  phoneNumber?: number;
+}
+
+export interface IdentifyResponse {
+  contact: {
+    primaryContactId: number;
+    emails: string[];
+    phoneNumbers: string[];
+    secondaryContactIds: number[];
+  };
+}
